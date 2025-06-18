626559b470f8d6ee9548793cb68994717737b8ba
Adam Mouflet <admoufle@f2r5s18.clusters.42paris.fr>
2025-06-18 19:15:40 +0200

    [MAJ]: syntaxe

--- srcs/parsing/syntaxe/check_syntaxe.c
+++ srcs/parsing/syntaxe/check_syntaxe.c
@@ -24,33 +24,45 @@ bool	is_cntl_op(enum e_token token)
 
 bool	is_syntaxe_ok(enum e_token prev, enum e_token token)
 {
-	if (is_cntl_op(prev) && is_cntl_op(token))
+	if (prev == open_par && (token == closing_par || is_cntl_op(token)))
+		return (false);
+	if (is_cntl_op(prev) && (is_cntl_op(token) || token == closing_par))
 		return (false);
 	if (is_redir(prev))
 		return (token == word);
+	if (prev == closing_par && token == word)
+		return (false);
 	return (true);
 }
 
 bool	check_syntaxe(t_snippet *lst)
 {
 	enum e_token	prev;
+	int				bracket;
 	char			*ptr;
 
 	prev = lst->token;
-	if (prev == semicolon || prev == pipe_delim || prev == closing_par || prev == or || prev == and)
+	if (is_cntl_op(prev) || prev == closing_par)
 		return (ft_printf("minishell: syntax error near unexpected token `%c'\n", lst->ptr[0]), false);
 	lst = lst->next;
 	ptr = NULL;
+	bracket = (int []){0, 1}[prev == open_par];
 	while (lst && is_syntaxe_ok(prev, lst->token))
 	{
+		if (lst->token == open_par)
+			bracket++;
+		else if (lst->token == closing_par)
+			bracket--;
 		prev = lst->token;
 		ptr = lst->ptr;
 		lst = lst->next;
 	}
+	if (bracket < 0)
+		return (ft_printf("minishell: syntax error near unexpected token `%c'\n", ')'), false);
 	if (lst)
-		return (ft_printf("minishell: syntax error near unexpected token `%c'\n", lst->ptr[0]), false);
+		return (ft_printf("minishell: syntax error near unexpected token `%s'\n", lst->ptr), false);
 	if (is_cntl_op(prev) || is_redir(prev) || (ptr && ptr[0] == '&'))
-		return (ft_printf("minishell: syntax error near unexpected token `%c'\n", ptr[0]), false);
+		return (ft_printf("minishell: syntax error near unexpected token `%s'\n", ptr), false);
 	return (true);
 }
 
